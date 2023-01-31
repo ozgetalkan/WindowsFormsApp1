d3c30a28f7cb4c9c2bb4fff5e2d3d12a1665e2f6
OzgeTanulku <ozgetanulkuu@gmail.com>
2023-01-31 11:29:20 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnGuessed = new System.Windows.Forms.Button();
+            this.txtGuessed = new System.Windows.Forms.TextBox();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(37, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(261, 340);
+            this.listBox1.TabIndex = 0;
+            // 
+            // btnGuessed
+            // 
+            this.btnGuessed.Enabled = false;
+            this.btnGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuessed.Location = new System.Drawing.Point(440, 170);
+            this.btnGuessed.Name = "btnGuessed";
+            this.btnGuessed.Size = new System.Drawing.Size(116, 35);
+            this.btnGuessed.TabIndex = 1;
+            this.btnGuessed.Text = "Check";
+            this.btnGuessed.UseVisualStyleBackColor = true;
+            this.btnGuessed.Click += new System.EventHandler(this.btnGuessed_Click);
+            // 
+            // txtGuessed
+            // 
+            this.txtGuessed.Enabled = false;
+            this.txtGuessed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuessed.Location = new System.Drawing.Point(400, 132);
+            this.txtGuessed.Name = "txtGuessed";
+            this.txtGuessed.Size = new System.Drawing.Size(196, 22);
+            this.txtGuessed.TabIndex = 2;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(410, 337);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(177, 83);
+            this.btnNewGame.TabIndex = 3;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.txtGuessed);
+            this.Controls.Add(this.btnGuessed);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnGuessed;
+        private System.Windows.Forms.TextBox txtGuessed;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random random= new Random();
+        string secretKey;
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] secretKeys = { "dolap", "bilgisayar", "fare", "kalem" };
+            secretKey = secretKeys[random.Next(0, 4)];
+            txtGuessed.Enabled = true;
+            btnGuessed.Enabled = true;
+            txtGuessed.Text = "";
+        }
+        private void btnGuessed_Click(object sender, EventArgs e)
+        {
+            string guess = txtGuessed.Text;
+            if (guess == secretKey)
+            {
+                MessageBox.Show("Kazandiniz");
+            }
+            else
+            {
+                char[] letters = guess.ToCharArray();
+                string matchedLetters = "";
+                foreach (char letter in letters) 
+                {
+                    if (secretKey.Contains(letter) && !matchedLetters.Contains(letter))
+                    {
+                        matchedLetters += $"{letter},";
+
+                    }
+                }
+                if (matchedLetters != "")
+                {
+                    matchedLetters = matchedLetters.TrimEnd(',');
+                    listBox1.Items.Add(matchedLetters);
+                }
+            }
+        }
+
+
+    }
+}
